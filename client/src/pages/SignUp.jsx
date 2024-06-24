bcf9fbb0acd79f881700d6af030dd19594f93eb1
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2024-06-24 13:56:17 +0530

    add functionality to the sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,49 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+const [formData, setFormData] = useState({});
+const [errorMessage,setErrorMessage] = useState(null);
+const [loading,setLoading] = useState(false);
+const navigate = useNavigate();
+  const handleChange =(e)=>{
+setFormData({...formData, [e.target.id]:e.target.value.trim()})
+  };
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please fill out all field');
+    }
+    try{
+      setLoading(true)
+      setErrorMessage(null)
+      console.log('Sending request to /api/auth/signup with:', formData);
+
+const res = await fetch('/api/auth/signup',{
+  method:"POST",
+  headers:{
+    "Content-Type":"application/json"
+  },
+  body:JSON.stringify(formData),
+});
+const data = await res.json();
+console.log('Response received:', data);
+
+if(data.success === false){
+  return setErrorMessage(data.message);
+}
+setLoading(false);
+if(res.ok){
+  navigate('/signin');
+}
+    }catch(error){
+setErrorMessage(error.message);
+setLoading(false);
+
+    }
+  }
   return (
     <div className='min-h-screen mt-20'>
       <div className=' flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -23,27 +64,39 @@ const SignUp = () => {
         </div>
         {/* right */}
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div >
               <Label value='Your username'/>
-             <TextInput type='text' placeholder='Username' id='username'/>
+             <TextInput type='text' placeholder='Username' id='username'onChange={handleChange}/>
             </div>
             <div >
               <Label value='Your email'/>
-             <TextInput type='text' placeholder='Email' id='email'/>
+             <TextInput type='email' placeholder='Email' id='email'onChange={handleChange}/>
             </div>
             <div >
               <Label value='Your password'/>
-             <TextInput type='text' placeholder='Password' id='password'/>
+             <TextInput type='password' placeholder='Password' id='password'onChange={handleChange}/>
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit'>
-              Sign Up
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+            {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
             </Button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an Account ?</span>
             <Link to='/signin' className='text-blue-500'>Sign In</Link>
           </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+proxy:{
+  '/api':{
+    target: 'http://127.0.0.1:3000',
+    changeOrigin: true, // Ensure this is set to true
+
+    secure:false,
+  },
+},
+  },
   plugins: [react()],
-})
+});
