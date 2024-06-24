016caf1e64dadc164365004d77675bdcdc5d563b
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2024-06-24 09:50:40 +0530

    add a middleware and a function to handle error

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,10 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup =async (req,res)=>{
+export const signup =async (req,res,next)=>{
 const {username,email,password} = req.body;
 if(!username || !email || !password || username === "" || email === "" || password === "") {
-  return res.status(400).json({message:"All field are requird"})
+next(errorHandler(400,'All field are required'))
 }
 
 const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -19,9 +20,7 @@ try{
   await newUser.save();
   res.json('register succesfully')
 }catch(error){
-res.status(500).json({message:error.message})
+next(error);
 }
-
-
 }
 
--- api/index.js
+++ api/index.js
@@ -21,3 +21,14 @@ app.listen(3000,()=>{
 })
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
+
+//error
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode ||500;
+  const message = err.message || 'Internal server error';
+  res.status(statusCode).json({
+    success :false,
+    statusCode,
+    message,
+  });
+})
