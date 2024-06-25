b2469ac48d259f6728b31981684a2258da0ca922
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2024-06-25 10:22:21 +0530

    make the dashboard private part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
  const App = () => {
   return (
@@ -16,7 +17,9 @@ import Footer from './components/Footer'
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
+      <Route element= {<PrivateRoute/>}>
       <Route path='/dashboard' element={<Dashboard/>}/>
+      </Route>
       <Route path='/projects' element={<Projects/>}/>
       <Route path='/signin' element={<SignIn/>}/>
       <Route path='/signup' element={<SignUp/>}/>
