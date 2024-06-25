129c2159ce1d674d7f5e4379229a53dba81996c2
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2024-06-25 14:01:58 +0530

    complete profile page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,8 +1,30 @@
+import { Button, TextInput } from 'flowbite-react'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const DashProfile = () => {
+
+  const {currentUser} = useSelector((state)=>state.user)
   return (
-    <div>DashProfile</div>
+    <div className='max-w-lg mx-auto p-3 w-full'>
+      <h1 className='my-7 text-center text-3xl font-semibold'>Profile</h1>
+      <form className='flex flex-col gap-6'>
+        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
+          <img src={currentUser.profilePicture} alt='user' className='rounded-full w-full h-full object-cover border-8 border-[lightgry]'></img>
+        </div>
+        <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}/>
+        <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}/>
+        <TextInput type='password' id='password' placeholder='password' />
+<Button type='submit' gradientDuoTone='purpleToBlue' outline>
+  Update
+</Button>
+      
+      </form>
+      <div className='text-red-500 flex justify-between mt-5'>
+        <span className='cursor-pointer'>Account Delete</span>
+        <span className='cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
   )
 }
 
