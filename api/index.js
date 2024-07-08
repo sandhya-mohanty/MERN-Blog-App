6c2ac8d37ada990eb81e3e88f18c1ce3136b4557
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2024-07-08 22:30:46 +0530

    add update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 
@@ -15,6 +16,7 @@ console.log(err)
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000,()=>{
   console.log('server running in port 3000 !!')
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { test } from '../controller/user.controller.js';
+import { test, updateUser } from '../controller/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router()
 
-router.get('/test',test)
+router.get('/test',test);
+router.put('/update/:userId',verifyToken, updateUser);
 export default router;
