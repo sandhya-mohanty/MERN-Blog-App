66dfda1ad4fb13ff677d729ae32548cd1e7d36b4
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2024-07-18 00:05:34 +0530

    add create post button in admin profile

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -7,11 +7,12 @@ import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import { deleteUserFailuer, deleteUserStart, deleteUserSuccess, signoutSuccess, updateFailuer, updateStart, updateSuccess } from '../redux/user/userReducer';
 import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { Link } from 'react-router-dom';
 
 
 const DashProfile = () => {
 
-  const {currentUser,error} = useSelector((state)=>state.user)
+  const {currentUser,error,loading} = useSelector((state)=>state.user)
 
   const [imageFile,setImageFile] = useState(null);
   const  [imageFileUrl,setImageFileUrl] = useState(null);
@@ -193,10 +194,17 @@ const handleSignout = async()=>{
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleChange}/>
         <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}/>
         <TextInput type='password' id='password' placeholder='password' onChange={handleChange} />
-<Button type='submit' gradientDuoTone='purpleToBlue' outline>
-  Update
+<Button type='submit' gradientDuoTone='purpleToBlue' outline disabled = {loading || imageFileUploading}>
+  {loading ? 'Loading...':'Update'}
 </Button>
-      
+    {currentUser.isAdmin && (
+      <Link to={'create-post'}>
+        <Button type='button' gradientDuoTone='purpleToPink'
+        className='w-full'>
+          Create a Post
+        </Button>
+      </Link>
+    )}  
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
         <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Account Delete</span>
