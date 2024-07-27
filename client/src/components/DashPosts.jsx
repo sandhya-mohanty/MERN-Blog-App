df626ea4f3bec4f46bc9d32a99ee0b3e1282502e
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2024-07-27 18:17:17 +0530

    add showmore functionality to the post result in dastboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,4 +1,4 @@
-import { Table } from 'flowbite-react';
+import { Button, Table } from 'flowbite-react';
 import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom';
@@ -7,6 +7,7 @@ import {  useSelector } from 'react-redux'
 const DashPosts = () => {
 const {currentUser} =useSelector((state)=>state.user);
 const [userPosts,setUserPosts] = useState([]);
+const [showMore,setShowMore] = useState(true);
 
   useEffect(() =>{
     const fetchPosts = async() =>{
@@ -15,6 +16,9 @@ const [userPosts,setUserPosts] = useState([]);
         const data = await res.json();
         if(res.ok){
          setUserPosts(data.posts);
+         if(data.posts.length < 9){
+          setShowMore(false);
+         }
         }
       } catch (error) {
         console.log(error.message);
@@ -24,8 +28,22 @@ const [userPosts,setUserPosts] = useState([]);
       fetchPosts();
     }
   },[currentUser._id])
+const handleShowMore = async() =>{
+const startIndex = userPosts.length;
+try {
+  const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+  const data = await res.json();
+  if(res.ok){
+    setUserPosts((prev) => [...prev,...data.posts])
+  };
+  if(data.posts.length < 9){
+    setShowMore(false);
+  }
+} catch (error) {
+  console.log(error.message);
+}
+}
 
-  
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
 {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -89,7 +107,9 @@ updatedAt).toLocaleDateString()}
       </Table.Body>
     ))}
   </Table>
-
+{showMore && (
+  <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>Show More</button>
+)}
   </>) :(
   <p>You have no posts yet!</p>
 )}
