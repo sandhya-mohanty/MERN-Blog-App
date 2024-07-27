8e7bd6b351c01531e25c156e3f54a73eb5bedfb5
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2024-07-27 23:22:16 +0530

    add update post ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Footer from './components/Footer'
 import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import CreatePost from './pages/CreatePost'
+import UpdatePost from './pages/UpdatePost'
 
  const App = () => {
   return (
@@ -24,6 +25,7 @@ import CreatePost from './pages/CreatePost'
       </Route>
       <Route element= {<OnlyAdminPrivateRoute/>}>
       <Route path='/create-post' element={<CreatePost/>}/>
+      <Route path='/update-post/:postId' element={<UpdatePost/>}/>
       </Route>
       <Route path='/projects' element={<Projects/>}/>
       <Route path='/signin' element={<SignIn/>}/>
