fb053852b723f0dd8be75045f45aaf89f7e46953
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2024-07-28 23:39:34 +0530

    create getall user api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, signout, test, updateUser } from '../controller/user.controller.js';
+import { deleteUser, getUser, signout, test, updateUser } from '../controller/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router()
@@ -8,4 +8,5 @@ router.get('/test',test);
 router.put('/update/:userId',verifyToken, updateUser);
 router.delete('/delete/:userId',verifyToken, deleteUser);
 router.post('/signout',signout);
+router.get('/getusers',verifyToken,getUser);
 export default router;
